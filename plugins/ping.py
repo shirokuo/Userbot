fec43e326f67b07a6b3685ca5bf000fe4505843a
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-10-24 15:45:44 +0700

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -2,6 +2,37 @@ import asyncio
 import os
 import sys
 import time
+from . import (
+    ATRA_COL,
+    LOGS,
+    OWNER_NAME,
+    OWNER_ID,
+    ULTROID_IMAGES,
+    Button,
+    Carbon,
+    Telegraph,
+    Var,
+    allcmds,
+    asst,
+    bash,
+    call_back,
+    callback,
+    def_logs,
+    eor,
+    get_string,
+    heroku_logs,
+    in_pattern,
+    inline_pic,
+    restart,
+    shutdown,
+    start_time,
+    time_formatter,
+    udB,
+    ultroid_bot,
+    ultroid_cmd,
+    ultroid_version,
+    updater,
+)
 
 
 @ultroid_cmd(pattern="iping$", chats=[], type=["official", "assistant"])

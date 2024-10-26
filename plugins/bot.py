fdf38190721af4b98795315e97e51c522a4349b4
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-10-26 17:15:16 +0700

    Update bot.py

--- plugins/bot.py
+++ plugins/bot.py
@@ -222,7 +222,7 @@ async def _(event):
     ment = await mention_user(user_id)
     prem = event.pattern_match.group(1)
     start = time.time()
-    x = await event.eor("ping")
+    x = await event.eor("🦋")
     end = round((time.time() - start) * 1000)
     uptime = time_formatter((time.time() - start_time) * 1000)
     if prem == "x":
@@ -231,7 +231,7 @@ async def _(event):
         await x.reply(get_string("iping").format(end))
     else:
         pic = udB.get_key("PING_PIC")
-        await asyncio.sleep(1)
+        await asyncio.sleep(5)
         await x.delete()
         await event.respond(get_string("ping").format(end, uptime, f"{ment}"), file=pic)
       

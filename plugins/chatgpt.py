fbc76e0b8a039a6d1f06280ad5f9d3900c9e584f
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-11-11 15:29:11 +0700

    Create chatgpt.py

---
+++ plugins/chatgpt.py
@@ -0,0 +1,26 @@
+import os
+import random
+import time
+#from Spotify_Music import app
+import requests
+from pyrogram.types import  Message
+from pyrogram.types import InputMediaPhoto
+from BadAPI import api
+from pyrogram.enums import ChatAction, ParseMode
+from pyrogram import filters
+
+
+@Client.on_message(filters.command("chatgpt"))
+async def chat_gpt(client, message):
+    
+    try:
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "Example:**\n\n/chatgpt Where is golden temple?")
+        else:
+            a = message.text.split(' ', 1)[1]
+            r=api.gemini(a)["results"]
+            await message.reply_text(f" {r} \n\n🌸 ᴘᴏᴡᴇʀᴇᴅ @PBX_PERMOT", parse_mode=ParseMode.MARKDOWN)     
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")

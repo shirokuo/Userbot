723d9f02ddea0d82163c688aa8ed025595e09e07
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-11-12 23:16:45 +0700

    Update chatgpt.py

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -5,7 +5,7 @@ from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters, Client 
 from MukeshAPI import api
 #@Mukesh.on_message(filters.command(["chatgpt","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
-
+from . import ultroid_cmd
 @ultroid_cmd(pattern="ask")
 async def chat_gpt(client, message): 
     try:

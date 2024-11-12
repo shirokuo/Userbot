d473912b2e6494b3567da1f690a44cde560162a9
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-11-12 23:09:20 +0700

    Update chatgpt.py

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -1,26 +1,22 @@
-import os
-import random
-import time
-#from Spotify_Music import app
 import requests
-from pyrogram.types import  Message
-from pyrogram.types import InputMediaPhoto
-from BadAPI import api
+#from .. import pbot as Mukesh,BOT_NAME,BOT_USERNAME
+import time
 from pyrogram.enums import ChatAction, ParseMode
-from pyrogram import filters
+from pyrogram import filters, Client 
+from MukeshAPI import api
+#@Mukesh.on_message(filters.command(["chatgpt","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
 
-
-@ultroid_cmd(pattern="ask( (.*)|$)")
-async def chat_gpt(client, message):
+@ultroid_cmd(pattern="ask")
+async def chat_gpt(bot, message):
     
     try:
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
             await message.reply_text(
-            "Example:**\n\n/chatgpt Where is golden temple?")
+            "Example:**\n\n`/chatgpt Where is TajMahal?`")
         else:
             a = message.text.split(' ', 1)[1]
             r=api.gemini(a)["results"]
-            await message.reply_text(f" {r} \n\n🌸 ᴘᴏᴡᴇʀᴇᴅ @PBX_PERMOT", parse_mode=ParseMode.MARKDOWN)     
+            await message.reply_text(f" {r} \n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @{BOT_USERNAME} ", parse_mode=ParseMode.MARKDOWN)     
     except Exception as e:
         await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")

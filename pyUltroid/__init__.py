844c1496e1b78f5c903d52dfbab190eef9ac6f0e
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-11-23 21:19:12 +0700

    Update __init__.py

--- pyUltroid/__init__.py
+++ pyUltroid/__init__.py
@@ -70,7 +70,7 @@ if run_as_module:
             validate_session(Var.SESSION, LOGS),
             udB=udB,
             app_version=ultroid_version,
-            device_model="Ultroid",
+            device_model="Userbot",
         )
         ultroid_bot.run_in_loop(autobot())
 
@@ -99,7 +99,7 @@ if run_as_module:
     DUAL_HNDLR = udB.get_key("DUAL_HNDLR") or "/"
     SUDO_HNDLR = udB.get_key("SUDO_HNDLR") or HNDLR
 else:
-    print("pyUltroid 2022 © TeamUltroid")
+    print("Userbot 2024 © Devikachan")
 
     from logging import getLogger
 

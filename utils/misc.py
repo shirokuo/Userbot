5edd42eb01b4e0d53b3a91e7f28d331673b11a39
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2024-12-02 22:57:38 +0700

    Update misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -34,7 +34,7 @@ requirements_list = []
 
 python_version = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
 
-prefix = db.get("core.main", "prefix", ".")
+#prefix = db.get("core.main", "prefix", ".")
 
 try:
     gitrepo = git.Repo(".")

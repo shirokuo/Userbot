f3406cbf9cc5c530309d974acf625e622ab1b787
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2024-12-14 14:07:27 +0700

    Update _decorators.py

--- pyUltroid/_misc/_decorators.py
+++ pyUltroid/_misc/_decorators.py
@@ -164,7 +164,7 @@ def ultroid_cmd(
                 LOGS.exception(e)
                 date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
                 naam = get_display_name(chat)
-                ftext = "**Userbot Client Error:** `Forward this to` @UltroidSupportChat\n\n"
+                ftext = "**Userbot Client Error:** `Forward this to` @xteam_cloner\n\n"
                 #ftext += "**Py-Ultroid Version:** `" + str(pyver)
                 #ftext += "`\n**Ultroid Version:** `" + str(ult_ver)
                 #ftext += "`\n**Telethon Version:** `" + str(telever)
@@ -194,7 +194,7 @@ def ultroid_cmd(
                         error_log = await asst.send_file(
                             udB.get_key("LOG_CHANNEL"),
                             file,
-                            caption="**Ultroid Client Error:** `Forward this to` @UltroidSupportChat\n\n",
+                            caption="**Userbot Client Error:** `Forward this to` @UltroidSupportChat\n\n",
                         )
                 else:
                     error_log = await asst.send_message(

361ffa58e3e59ebd6656ff92feb2b2f358612dbd
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2024-12-17 01:16:22 +0700

    Update ping.py

--- modules/ping.py
+++ modules/ping.py
@@ -1,41 +1,53 @@
+# UserBot
+# Copyright (C) 2021-2023 Teamx-cloner 
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+from . import get_help
+
+__doc__ = get_help("help_bot")
+
 import asyncio
 import os
 import sys
 import time
-from . import (
-    ATRA_COL,
-    LOGS,
-    OWNER_NAME,
-    OWNER_ID,
-    ULTROID_IMAGES,
-    Button,
-    Carbon,
-    Telegraph,
-    Var,
-    allcmds,
-    asst,
-    bash,
-    call_back,
-    callback,
-    def_logs,
-    eor,
-    get_string,
-    heroku_logs,
-    in_pattern,
-    inline_pic,
-    restart,
-    shutdown,
-    start_time,
-    time_formatter,
-    udB,
-    ultroid_bot,
-    ultroid_cmd,
-    ultroid_version,
-    updater,
-)
+from telethon.tl.functions import PingRequest
+from pyUltroid.fns.custom_markdown import CustomMarkdown
+from . import OWNER_NAME, start_time, time_formatter, udB, ultroid_cmd as xteam_cmd, get_string
+
+async def mention_user(user_id):
+    try:
+        user_entity = await ultroid_bot.get_entity(user_id)
+        first_name = user_entity.first_name
+        mention_text = f"[{first_name}](tg://user?id={user_id})"
+        return mention_text
+    except Exception as e:
+        print(f"Failed to mention user: {e}")
+
+@xteam_cmd(pattern="ping(|x|s)$", chats=[], type=["official", "assistant"])
+async def _(event):
+    ultroid_bot.parse_mode = CustomMarkdown()
+    user_id = OWNER_ID
+    ment = await mention_user(user_id)
+    prem = event.pattern_match.group(1)
+    start = time.time()
+    x = await event.eor("ping")
+    end = round((time.time() - start) * 1000)
+    uptime = time_formatter((time.time() - start_time) * 1000)
+    if prem == "x":
+        await x.reply(get_string("pping").format(end, uptime))
+    elif prem == "s":
+        await x.reply(get_string("iping").format(end))
+    else:
+        pic = udB.get_key("PING_PIC")
+        await asyncio.sleep(1)
+        await x.delete()
+        await event.respond(get_string("ping").format(end, uptime, f"{OWNER_NAME}"), file=pic)
 
 
-@ultroid_cmd(pattern="Ping$", chats=[], type=["official", "assistant"])
+@xteam_cmd(pattern="Ping$", chats=[], type=["official", "assistant"])
 async def _(event):
     start = time.time()
     x = await event.reply("Ping")

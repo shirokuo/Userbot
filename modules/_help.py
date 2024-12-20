3fa85a8fec750315e3ac40007b70fe8bda60d61a
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2024-12-20 17:21:53 +0700

    Update _help.py

--- modules/_help.py
+++ modules/_help.py
@@ -45,19 +45,19 @@ async def _help(ult):
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["Official"][plug]:
                     output += i
-                output += "\n© @Error405RequestTimeOut"
+                output += "\n© @xteam_cloner"
                 await ult.eor(output)
             elif HELP.get("Addons") and plug in HELP["Addons"]:
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["Addons"][plug]:
                     output += i
-                output += "\n© @Error405RequestTimeOut"
+                output += "\n© @xteam_cloner"
                 await ult.eor(output)
             elif HELP.get("VCBot") and plug in HELP["VCBot"]:
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["VCBot"][plug]:
                     output += i
-                output += "\n© @Error405RequestTimeOut"
+                output += "\n© @xteam_cloner"
                 await ult.eor(output)
             else:
                 try:
@@ -65,7 +65,7 @@ async def _help(ult):
                     for d in LIST[plug]:
                         x += HNDLR + d
                         x += "\n"
-                    x += "\n© @Error405RequestTimeOut"
+                    x += "\n© @xteam_cloner"
                     await ult.eor(x)
                 except BaseException:
                     file = None
@@ -100,7 +100,7 @@ async def _help(ult):
                     elif HELP.get("VCBot") and file in HELP["VCBot"]:
                         for i in HELP["VCBot"][file]:
                             output += i
-                    output += "\n© @Error405RequestTimeOut"
+                    output += "\n© @xteam_cloner"
                     await ult.eor(output)
         except BaseException as er:
             LOGS.exception(er)
